8aa6104a4f6bfc59a9200b724d5a0e30f1f9a477
EmeraldPangolin <meyerdeavan29205@gmail.com>
2023-04-25 18:57:19 -0500

    updating imports for createNote

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import React, {useEffect, useReducer} from 'react'
 import { API } from 'aws-amplify';
-import { List } from 'antd';
 import 'antd/dist/antd.css';
+import { v4 as uuid } from 'uuid'
+import { List, Input, Button } from 'antd'
 import { listNotes } from './graphql/queries';
+import { createNote as CreateNote } from './graphql/mutations'
 
 const initialState = {
   notes: [],
